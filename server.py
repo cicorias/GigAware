d30235b4e26be3c896c124f2b85ceac145dfb3b6
alenakru <alenavkruchkova@gmail.com>
2016-05-22 05:34:44 -0700

    Create app route for user mode page and task-setup page

--- server.py
+++ server.py
@@ -13,3 +13,21 @@ def landing_page():
 def login():
     """Login with facebook facebook oauth. Maybe hardcoded for the demo"""
 
+@app.route('location')
+def location():
+    """Allow geolocation or grab geolocation from user input(address)"""
+
+@app.route('usermode')
+def usermode():
+    """Select type of user. Form with checkbox input"""
+
+@app.route('task_setup')
+def login():
+    """Form filled out by employer.
+
+        Describe task. Taggs(possibly 2.0 or hardcoded)
+        Sliding scale for the next 7 days,
+        to specify days and times when task can be accomplished
+        Checkboxes for food and pcik-up/drop-off availability
+        Compensation is specified.
+        """
